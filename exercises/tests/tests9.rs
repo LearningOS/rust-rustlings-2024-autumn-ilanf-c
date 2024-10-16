f58abf044cbabd6deb9354631e85707362279df7
ilanf <willowilanf@gmail.com>
2024-10-16 23:07:27 +0800

    test exercises finished

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -1,9 +1,10 @@
 // tests9.rs
 //
-// Rust is highly capable of sharing FFI interfaces with C/C++ and other statically compiled
-// languages, and it can even link within the code itself! It makes it through the extern
-// block, just like the code below.
+// Rust is highly capable of sharing FFI（外部函数接口：允许一种编程语言调用另一种语言编写的函数或使用其库）
+// interfaces with C/C++ and other statically compiled languages, and it can even link within
+// the code itself! It makes it through the extern block, just like the code below.
 //
+// ABI：应用二进制接口
 // The short string after the `extern` keyword indicates which ABI the externally imported
 // function would follow. In this exercise, "Rust" is used, while other variants exists like
 // "C" for standard C ABI, "stdcall" for the Windows ABI.
@@ -18,7 +19,7 @@
 // for Rust functions is literally "Rust", so if you want to link against pure Rust functions,
 // the whole extern term can be omitted.
 //
-// Rust mangles symbols by default, just like C++ does. To suppress this behavior and make
+// Rust mangles（混淆） symbols by default, just like C++ does. To suppress this behavior and make
 // those functions addressable by name, the attribute #[no_mangle] can be applied.
 //
 // In this exercise, your task is to make the testcase able to call the `my_demo_function` in
@@ -26,16 +27,17 @@
 // line of code in the testcase should call the same function.
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
-
-// I AM NOT DONE
-
+// 在 Rust 中，编译器默认会对函数名进行名称混淆（也称为“符号重整”），这是为了确保不同模块中的函数名不会冲突
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
+    #[no_mangle]
+    // 告诉编译器不要修改这个函数的名称，使得它可以在外部以原始名字引用。允许你为函数指定一个不同的符号名称。
     fn my_demo_function(a: u32) -> u32 {
         a
     }
@@ -54,6 +56,7 @@ mod tests {
         // SAFETY: We know those functions are aliases of a safe
         // Rust function.
         unsafe {
+            // 在unsafe中调用，rust默认外部代码是不安全的
             my_demo_function(123);
             my_demo_function_alias(456);
         }
